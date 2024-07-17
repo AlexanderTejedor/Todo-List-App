9c51c60b46dbda430819478f6e069cd959ce26b8
Alexander Tejedor <atromero.98@gmail.com>
2024-07-17 08:30:58 -0500

    movimiento de carpetas

--- src/TodoDate.js
+++
@@ -1,7 +0,0 @@
-import './css/style.css'
-function TodoDate () {
-    return (
-        <p className='todo-date'>July 11, 2024</p>
-    )
-}
-export {TodoDate}
--- src/TodoDay.js
+++
@@ -1,7 +0,0 @@
-import './css/style.css'
-function TodoDay(){
-    return(
-        <h1 className='todo-day'>Thursday</h1>
-    )
-}
-export {TodoDay}
