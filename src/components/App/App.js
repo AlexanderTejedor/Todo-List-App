ae93f19326e13c0dea32c6725462d73f78997754
Alexander Tejedor <atromero.98@gmail.com>
2026-03-06 10:26:04 -0500

    refactor(ui): encapsulate context logic within custom hook

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,77 @@
-import React from 'react';
-import { AppUI } from './AppUI';
-import { TodoProvider } from '../TodoContext/TodoContext';
+import React from "react";
+import { useTodos } from "./useTodos.js";
+import { TodoCount } from "../TodoCount/TodoCount";
+import { TodoSearch } from "../TodoSearch/TodoSeach";
+import { TodoList } from "../TodoList/TodoList";
+import { TodoItem } from "../TodoItem/TodoItem";
+import { TodoButtom } from "../TodoButtom/TodoButton";
+import { TodoForm } from "../TodoForm/TodoForm";
+import { TodoDay } from "../TodoDate/TodoDay";
+import { TodoDate } from "../TodoDate/TodoDate";
+import { TodosLoading } from "../TodosLoading/TodosLoading";
+import { TodosError } from "../TodosError/TodosError";
+import { EmptyTodos } from "../EmptyTodos/EmptyTodos";
+import { Modal } from "../Modal/Modal";
+import { TodoHeader } from "../TodoHeader/TodoHeader";
 
 function App() {
+  const {
+    loading,
+    error,
+    searchedTodos,
+    completeTodo,
+    deleteTodo,
+    openModal,
+    setOpenModal,
+    totalTodos,
+    completedTodos,
+    searchValue,
+    setSearchValue,
+    addTodo,
+  } = useTodos();
+
   return (
-    <TodoProvider>
-      <AppUI />
-    </TodoProvider>
-  )
-  
+    <main className="main-container">
+      <section className="todo-content">
+        <div className="todo-date__content">
+          {/* header */}
+          <TodoDay />
+          <TodoDate />
+        </div>
+        <TodoHeader>
+          <TodoCount totalTodos={totalTodos} completedTodos={completedTodos} />
+          {/* input */}
+          <TodoSearch
+            searchValue={searchValue}
+            setSearchValue={setSearchValue}
+          />
+        </TodoHeader>
+        {/* list */}
+        <TodoList>
+          {loading && <TodosLoading />}
+          {error && <TodosError />}
+          {!loading && searchedTodos.length === 0 && <EmptyTodos />}
+          {searchedTodos.map((todo) => (
+            <TodoItem
+              key={todo.text}
+              text={todo.text}
+              completed={todo.completed}
+              onComplete={() => completeTodo(todo.text)}
+              onDelete={() => deleteTodo(todo.text)}
+            />
+          ))}
+        </TodoList>
+        {/* Buttom */}
+        <TodoButtom setOpenModal={setOpenModal} />
+
+        {openModal && (
+          <Modal>
+            <TodoForm addTodo={addTodo} setOpenModal={setOpenModal} />
+          </Modal>
+        )}
+      </section>
+    </main>
+  );
 }
 
 export default App;
